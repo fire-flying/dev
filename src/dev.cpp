305bdd0f514f1bbcbe4c3c7ad3b350ce5fe3fae8
sample <sample@sample.com>
2022-02-23 09:18:11 +0800

    winapi test!

--- src/dev.cpp
+++ src/dev.cpp
@@ -1,9 +1,11 @@
 ﻿
 #include <iostream>
+#include <windows.h>
 
 int main()
 {
     std::cout << "Hello World!\n";
+    MessageBoxW(NULL,TEXT("test bopx"),TEXT("caption"),SW_SHOW);    
     std::cout << "Hello World!\n";
     std::cout << "Hello World!\n";
     return 0;
