e1818b52cf6a1d8aeabd28f528d7f495cd9568c6
Fire <52570947+fire-flying@users.noreply.github.com>
2022-04-28 14:32:17 +0800

    update

--- src/dev.cpp
+++ src/dev.cpp
@@ -4,10 +4,10 @@
 
 int main()
 {
-    // std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
     MessageBoxW(NULL,TEXT("test bopx"),TEXT("caption"),SW_SHOW);    
-    // std::cout << "Hello World!\n";
-    // std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
     return 0;
 }
 
